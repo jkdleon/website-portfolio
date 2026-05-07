b7496b9b9e6e726011aa70dee9cb765e5da244b8
James Kyle De Leon <jameskyle.dleon@gmail.com>
2026-05-07 17:50:06 +0300

    feat/updated certification, education, experience

--- src/lib/constants/education.ts
+++ src/lib/constants/education.ts
@@ -1,3 +1,6 @@
 import { EducationEntry } from './types';
 
-export const education: EducationEntry[] = [];
+export const education: EducationEntry[] = [
+    { degree: 'Bachelor of Science in Electronics Engineering', institution: ' National University Manila', dates: '2014 - 2016' },
+    { degree: 'Bachelor of Science in Electronics and Communication Engineering Technology', institution: 'Rizal Technological University', dates: '2011 - 2014' },
+];
